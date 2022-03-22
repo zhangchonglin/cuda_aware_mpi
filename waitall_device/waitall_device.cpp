a73c3995605ea5a176275d1ef1dae4bc3f736508
Chonglin Zhang <zhangchonglin@gmail.com>
2022-03-22 14:30:45 -0700

    update example case

--- waitall_device/waitall_device.cpp
+++ waitall_device/waitall_device.cpp
@@ -9,32 +9,6 @@ int main(int argc, char *argv[]) {
     int myrank;
     MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
 
-    float *val_host, *val_device;
-    val_host = (float*)malloc(sizeof(float));
-    cudaMalloc((void **)&val_device, sizeof(float));
-
-    *val_host = -1.0;
-    if (myrank != 0) {
-      printf("%s %d %s %f\n", "I am rank", myrank, "and my initial value is:", *val_host);
-    }
-
-    if (myrank == 0) {
-        *val_host = 42.0;
-        cudaMemcpy(val_device, val_host, sizeof(float), cudaMemcpyHostToDevice);
-        printf("%s %d %s %f\n", "I am rank", myrank, "and will broadcast value:", *val_host);
-    }
-
-    MPI_Bcast(val_device, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
-
-    if (myrank != 0) {
-      cudaMemcpy(val_host, val_device, sizeof(float), cudaMemcpyDeviceToHost);
-      printf("%s %d %s %f\n", "I am rank", myrank, "and received broadcasted value:", *val_host);
-    }
-
-    cudaFree(val_device);
-    free(val_host);
-
-
     // send and receive example
     int comm_size;
     MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
